247c2fdcc782187f865f041ccf4dc329ebe84ec2
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2024-04-11 23:44:47 +0530

    Update internet_speed_test.py

--- internet_speed_test.py
+++ internet_speed_test.py
@@ -1,6 +1,7 @@
-#Internet Speed Test using python
+#Internet Speed Test using Python
 
 import speedtest
+
 wifi  = speedtest.Speedtest()
 print("Wifi Download Speed is ", wifi.download())
 print("Wifi Upload Speed is ", wifi.upload())
